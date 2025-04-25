be300e35b4de80820e03ed9bc3937b770e863ba2
Hanxmao <hanxmao@hotmail.com>
2025-04-25 01:07:24 -0700

    add formsubmit service

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,36 +52,3 @@ for (let i = 0, len = revealDelayElements.length; i < len; i++) {
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
-
-// ------------emailJS------------------
-const contactForm = document.getElementById("contact-form"),
-  contactName = document.getElementById("contact-name"),
-  contactEmail = document.getElementById("contact-email"),
-  contactMessage = document.getElementById("contact-message"),
-  contactStatus = document.getElementById("contact-status")
-
-const sendEmail = (event) => {
-  event.preventDefault();
-
-  if (contactName.value === "" || contactEmail.value === "" || contactMessage.value === "") {
-    contactStatus.classList.remove("color-blue");
-    contactStatus.classList.add("color-red");
-
-    contactMessage.textContent = "All input fields are required"
-  } else {
-    // serviceID, templateID, formID, publicKey
-    emailjs.sendForm('service_rfa6leo', 'template_bbhzego', '#contact-form', 'ujZodUI_1SupV9PPu')
-      .then((res) => {
-        console.log(res);
-        contactStatus.classList.add('color-blue')
-        contactStatus.textContent = "Message sent successfully "
-        setTimeout(() => {
-          contactStatus.textContent = ""
-        }, 5000)
-      }, (err) => {
-        alert("Something went wrong", err)
-      })
-  }
-}
-
-contactForm.addEventListener('submit', sendEmail)
